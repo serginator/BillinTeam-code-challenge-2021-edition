2fb9d1fa6d6215fbcb5822a65d9dd28c2948a9ea
Sergio Ruiz <serginator@gmail.com>
2022-10-08 22:09:38 +0200

    define types

---
+++ src/types.ts
@@ -0,0 +1,34 @@
+type Status = 'issued' | 'draft';
+
+type Invoice = {
+  code: string;
+  issuedDate: string;
+  ownerName: string;
+  contactName: string;
+  subtotal: number;
+  taxes: number;
+  total: number;
+  status: Status;
+};
+
+type PropertyError = {
+  property: string;
+  message: string;
+};
+
+type InvoiceError = {
+  line: number;
+  errors: PropertyError[] | boolean;
+};
+
+type ImportResult = {
+  ok: Invoice[];
+  ko: InvoiceError[];
+};
+
+export {
+  Invoice,
+  PropertyError,
+  InvoiceError,
+  ImportResult,
+};
