4684d53094971cf9ec064f82dce6fc0357620e0e
Sergio Ruiz <serginator@gmail.com>
2022-10-08 22:38:52 +0200

    make importer read and parse csv file and process it

--- src/importer.ts
+++ src/importer.ts
@@ -1,12 +1,43 @@
+import fs from 'fs';
+import path from 'path';
+import { parse } from 'csv-parse';
+import { Invoice, InvoiceError, ImportResult } from './types';
+import { validate } from './validate';
+
 export class Importer {
-  async import(filePath: string): Promise<any> {
-    console.log(
-      'remove this line when you use the filePath variable',
-      filePath,
-    );
-    return {
-      ok: [],
-      ko: [],
-    };
+  async import(filePath: string): Promise<ImportResult> {
+    const ok: Invoice[] = [];
+    const ko: InvoiceError[] = [];
+    try {
+      const file = path.join(__dirname, '..', 'files', filePath);
+      const fileContent = fs.readFileSync(file, 'utf8');
+      const headers = ['code', 'issuedDate', 'ownerName', 'contactName', 'subtotal', 'taxes', 'total', 'status'];
+
+      const invoices = parse(fileContent, {
+        delimiter: ';',
+        columns: headers,
+        fromLine: 2,
+        trim: true,
+        cast: (value, context) => {
+          if (context.column === 'subtotal' || context.column === 'taxes' || context.column === 'total') {
+            return Number(value);
+          }
+          return value;
+        }
+      });
+      let line = 1;
+      for await (const invoice of invoices) {
+        const errors = validate(invoice);
+        errors ? ko.push({line: line, errors: errors}) : ok.push(invoice);
+        line++;
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      return {
+        ok,
+        ko
+      };
+    }
   }
 }
