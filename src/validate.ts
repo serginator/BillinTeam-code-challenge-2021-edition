7492b2cb7dfc688188cc40fa6cb17e9f00931d28
Sergio Ruiz <serginator@gmail.com>
2022-10-08 22:33:02 +0200

    add validate method to check an Invoice

---
+++ src/validate.ts
@@ -0,0 +1,55 @@
+import { Invoice, PropertyError } from './types'
+
+export function validate(invoice: Invoice): PropertyError[] | boolean {
+    const errors: PropertyError[] = [];
+
+    if (invoice.code === '') {
+        errors.push({
+            property: 'code',
+            message: 'required'
+        });
+    }
+    if (!/^\d{4}\-\d{1,2}\-\d{1,2}$/.test(invoice.issuedDate)) {
+        errors.push({
+            property: 'issuedDate',
+            message: 'invalid'
+        });
+    }
+    if (invoice.ownerName === '') {
+        errors.push({
+            property: 'ownerName',
+            message: 'required'
+        });
+    }
+    if (invoice.contactName === '') {
+        errors.push({
+            property: 'contactName',
+            message: 'invalid'
+        });
+    }
+    if (isNaN(invoice.subtotal)) {
+        errors.push({
+            property: 'subtotal',
+            message: 'invalid'
+        });
+    }
+    if (isNaN(invoice.taxes)) {
+        errors.push({
+            property: 'taxes',
+            message: 'invalid'
+        });
+    }
+    if (isNaN(invoice.total)) {
+        errors.push({
+            property: 'total',
+            message: 'invalid'
+        });
+    }
+    if (!['issued', 'draft'].includes(invoice.status)) {
+        errors.push({
+            property: 'status',
+            message: 'invalid'
+        });
+    }
+    return errors.length ? errors : false;
+}
